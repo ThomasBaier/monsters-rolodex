e8ab2fd088cfc34907a3bb5b1aa3f5c8ee39a314
Thomas <th.baier@posteo.de>
2020-03-20 18:38:42 +0100

    Updates

---
+++ precache-manifest.bae80963ee50e2862afe8cafeccb580f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bd98db91b502be9f5c0ed8d458e7f62",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f82a8fc53cb4ca41de23",
+    "url": "/monsters-rolodex/static/css/main.e2497999.chunk.css"
+  },
+  {
+    "revision": "7270fc78a3ec7a388d29",
+    "url": "/monsters-rolodex/static/js/2.a6ed1c5a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a6ed1c5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f82a8fc53cb4ca41de23",
+    "url": "/monsters-rolodex/static/js/main.49600909.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
